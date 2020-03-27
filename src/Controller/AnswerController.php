b56e36fda9aa6d250e38962a500864d0f1a13f3e
quandt2 <doanq@UITDOANQMBP.local>
2020-03-27 18:25:51 -0400

    Modify nelmio API

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -39,14 +39,40 @@ class AnswerController extends AbstractController
     }
 
     /**
+     * Create the answer
+     *
      * @Route("/createAnswer", name="createAnswer", methods={"POST"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the detail of created ",
+     *     @SWG\Schema(
+     *         @SWG\Property(property="id", type="integer", description="UUID"),
+     *         @SWG\Property(property="answer_id", type="integer"),
+     *         @SWG\Property(property="answer", type="string"),
+     *         @SWG\Property(property="tags", type="string")
+     *     )
+     * )
+     * @SWG\Parameter(
+     *         name="question",
+     *         in="body",
+     *         type="string",
+     *         description="Enter the question info",
+     *         required=true,
+     *          @SWG\Schema(
+     *             @SWG\Property(property="id", type="integer", description="UUID"),
+     *             @SWG\Property(property="answer_id", type="integer"),
+     *             @SWG\Property(property="answer", type="string"),
+     *             @SWG\Property(property="tags", type="string"),
+     *             @SWG\Property(property="question_id", type="integer")
+     *     )
+     * )
+     *
      */
     public function createAnswer(Request $request)
     {
         try {
             $data = json_decode($request->getContent(), true);
-
-            $this->logger->info(\GuzzleHttp\json_encode($data));
             $question_id = $data["question_id"];
             $question = $this->questionRepository->find($question_id);
             if ($question == null) {
@@ -58,8 +84,9 @@ class AnswerController extends AbstractController
             return new JsonResponse([
                 "success" => "question is created",
                 "detail" => [
+                    "id" => $answerObj->getId(),
                     "answer_id" => $answerObj->getAnswerId(),
-                    "question" => $answerObj->getAnswer(),
+                    "answer" => $answerObj->getAnswer(),
                     "tags" => $answerObj->getTags()
                 ]], Response::HTTP_CREATED);
 
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -29,12 +29,50 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * get the list of question and its answer
      * @Route("/question", name="question", methods={"POST"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the detail of created ",
+     *     @SWG\Schema(
+     *         @SWG\Property(property="id", type="integer", description="UUID"),
+     *             @SWG\Property(property="question", type="string"),
+     *             @SWG\Property(property="rank", type="integer")
+     *
+     *     )
+     * )
+     * @SWG\Parameter(
+     *          name="body",
+     *          in="body",
+     *          required=true,
+     *          @SWG\Schema(
+     *
+     *
+     *              @SWG\Property(
+     *                  property="search",
+     *
+     *                  @SWG\Property(property="question", type="string", description = "search by question"),
+     *                  @SWG\Property(property="rank", type="integer", description = "search by rank"),
+     *              ),
+     *              @SWG\Property(
+     *                  property="orderBy",
+     *                  @SWG\Property(property="id", type="integer", description="can be null/ASC/DESC"),
+     *                  @SWG\Property(property="rank", type="string", description="can be null/ASC/DESC"),
+     *              ),
+     *              @SWG\Property(property="itemPerPage",  type="integer", description="used for pagination" ),
+     *              @SWG\Property(property="pageNumber",  type="integer",  description="used for pagination"),
+     *
+     *          ),
+     *      ),
      */
     public function index(Request $request)
     {
         try {
             $data = json_decode($request->getContent(), true);
+
+            $this->logger->info("input data");
+            $this->logger->info(json_encode($data));
             $search = $data["search"];
             $orderBy = !empty($data["orderBy"]) ? $data["orderBy"] : ["id" => "ASC"];
             //$pagination = $data["pagination"];
@@ -76,27 +114,25 @@ class QuestionController extends AbstractController
      *
      * @SWG\Response(
      *     response=200,
-     *     description="Returns the rewards of an user",
+     *     description="Returns the detail of created ",
      *     @SWG\Schema(
-     *         type="array",
-     *         @SWG\Items(ref=@Model(type=Question::class))
+     *         @SWG\Property(property="id", type="integer", description="UUID"),
+     *             @SWG\Property(property="question", type="string"),
+     *             @SWG\Property(property="rank", type="integer")
      *     )
      * )
      * @SWG\Parameter(
      *         name="question",
-     *         in="formData",
+     *         in="body",
      *         type="string",
-     *         description="Enter the question"
-     *         required=true
-     *
+     *         description="Enter the question info",
+     *         required=true,
+     *          @SWG\Schema(
+     *             @SWG\Property(property="question", type="string"),
+     *             @SWG\Property(property="rank", type="integer")
+     *     )
      * )
      *
-     * @SWG\Parameter(
-     *         name="rank",
-     *         in="formData",
-     *         type="string",
-     *         description="Enter the rank of the question"
-     * )
      */
     public function create(Request $request) : JsonResponse
     {
@@ -115,12 +151,13 @@ class QuestionController extends AbstractController
             return new JsonResponse([
                 "success" => "question is created",
                 "detail" => [
+                    "id" => $questionObj->getId(),
                     "question" => $questionObj->getQuestion(),
                     "rank" => $questionObj->getRank()
                 ]], Response::HTTP_CREATED);
 
         } catch (\Exception $e) {
-            return new JsonResponse(["error" => $e->getMessage()], 500);
+            return new JsonResponse(["error" => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
